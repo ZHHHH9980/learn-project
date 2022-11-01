5843a6c2a280db51e90d9d295ad16a18ae7d7d02
zhonghao <zhonghao.0930@bytedance.com>
2022-11-01 10:51:52 +0800

    feat: add package.json

--- udemy/node-complete-guide/08.first_express_api/src/controller/messages.controller.js
+++ udemy/node-complete-guide/08.first_express_api/src/controller/messages.controller.js
@@ -1,8 +1,7 @@
 const path = require('path');
 
 const getMessages = (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', '096-skimountain.jpg'));
+};
 
-    res.sendFile(path.join(__dirname, '..', 'public', '096-skimountain.jpg'));
-}
-
-module.exports =  {getMessages};
+module.exports = { getMessages };
--- udemy/node-complete-guide/09.nasa-project/server/src/app.js
+++ udemy/node-complete-guide/09.nasa-project/server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.route');
+const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -12,6 +14,14 @@ app.use(
   })
 );
 
+app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
